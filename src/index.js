aa2323c2f6d621a9d40c0856934e22aa7de5f732
SamheinDM <SamheinDM@yandex.ru>
2020-09-30 19:07:03 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === [] || array == undefined) return 0;
+  let result = array.sort(compare);
+  function compare(a, b) {
+    return a-b;
+  }
+  return result[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === [] || array == undefined) return 0;
+  let result = array.sort(compare);
+  function compare(a, b) {
+    return a-b;
+  }
+  result.reverse();
+  return result[0];
 }
 
 exports.avg = function avg (array) {
